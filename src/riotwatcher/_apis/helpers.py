3df338a2a7a386bd9fed8b8cd4d71c8ce3b60939
danishs-lab <danish.samir@ethara.ai>
2026-04-24 16:55:13 +0530

    Add spec PDF for Riot-Watcher

--- src/riotwatcher/_apis/helpers.py
+++ src/riotwatcher/_apis/helpers.py
@@ -2,7 +2,4 @@ from functools import wraps
 
 
 def remap_region_to_platform(argument_pos: int, argument_name="region"):
-    def _remap_region_to_platform(api_call):
-        pass
-
-    return _remap_region_to_platform
+    pass
